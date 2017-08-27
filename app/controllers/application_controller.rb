51ff5eee129b67762b83d96a3e24ee8713e73241
Yevhen Radionov <eugene.radionov@gmail.com>
2017-08-27 13:22:19 +0300

    Added cancan exception handling.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,4 +2,11 @@ class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
   include CurrentCart
   before_action :set_cart
+
+  rescue_from CanCan::AccessDenied do |exception|
+    respond_to do |format|
+      format.json { head :forbidden }
+      format.html { redirect_to root_path, alert: exception.message }
+    end
+  end
 end
