e964aef990510cd9a804635263388b5b4a98bfa3
Yevhen Radionov <eugene.radionov@gmail.com>
2017-08-27 16:29:59 +0300

    Bug fixes.

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -4,6 +4,7 @@ RSpec.describe User, type: :model do
   let(:user) { FactoryGirl.build(:user) }
   let(:user_pass_no_number) { FactoryGirl.build(:user, password: 'qwertyQwerty') }
   let(:user_pass_no_upper_letter) { FactoryGirl.build(:user, password: 'qwertyq123') }
+  let(:user_short_pass) { FactoryGirl.build(:user, password: 'wertyQ1') }
 
   it { expect(user).to validate_presence_of :email }
   xit { expect(user).to validate_uniqueness_of :email } # TODO: fix validation
@@ -19,12 +20,17 @@ RSpec.describe User, type: :model do
   end
 
   it 'does not save password without at least one uppercase letter' do
-    expect(user_pass_no_number).not_to be_valid
-    user_pass_no_number.valid?
+    expect(user_pass_no_upper_letter).not_to be_valid
+    user_pass_no_upper_letter.valid?
+  end
+
+  it 'does not save password with password length < 8' do
+    expect(user_short_pass).not_to be_valid
+    user_short_pass.valid?
   end
 
   it 'save user with valid password' do
     expect(user).to be_valid
-    user_pass_no_number.valid?
+    user.valid?
   end
 end
