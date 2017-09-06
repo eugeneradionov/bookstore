9a854b48948f6e392e1d460c7cd8736417985942
Yevhen Radionov <eugene.radionov@gmail.com>
2017-09-06 18:51:24 +0300

    Added rating input to review.

--- app/models/review.rb
+++ app/models/review.rb
@@ -3,6 +3,8 @@ class Review < ApplicationRecord
   belongs_to :user
   belongs_to :review_status
   validates_presence_of :title, :text
+  validates :rating, inclusion: { in: 0..5,
+                                  message: 'is not in 0-5' }
 
   scope :approved, -> { where(review_status: ReviewStatus.find_by(status: 'Approved')) }
   scope :unprocessed, -> { where(review_status: ReviewStatus.find_by(status: 'Unprocessed')) }
