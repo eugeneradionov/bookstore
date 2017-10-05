244df6720e47d3875afc7a8950ad40796dac8917
Yevhen Radionov <eugene.radionov@gmail.com>
2017-10-05 16:41:17 +0300

    Fixed inability to visit admin path.

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
-  devise_for :admins, controllers: { sessions: 'admins/sessions' }
+  devise_for :admin
   mount RailsAdmin::Engine => '/admin', as: 'rails_admin'
 
   resources :reviews, only: [:create, :destroy]
