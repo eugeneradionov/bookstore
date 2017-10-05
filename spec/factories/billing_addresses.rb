f5e89dfae236c11396922732424e3e8cfb89650d
Yevhen Radionov <eugene.radionov@gmail.com>
2017-10-05 16:12:17 +0300

    Added tests for Ability.

--- spec/factories/billing_addresses.rb
+++ spec/factories/billing_addresses.rb
@@ -1,11 +1,11 @@
 FactoryGirl.define do
   factory :billing_address do
-    first_name "MyString"
-    last_name "MyString"
-    address "MyString"
-    city "MyString"
-    zip "MyString"
-    country "MyString"
-    phone "MyString"
+    first_name 'MyString'
+    last_name 'MyString'
+    address 'MyString'
+    city 'MyString'
+    zip 1234
+    country 'US'
+    phone '+380 33 321 12 34'
   end
 end
